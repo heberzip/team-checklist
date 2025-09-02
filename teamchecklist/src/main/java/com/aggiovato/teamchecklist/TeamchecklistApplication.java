5036a99643400476463ac1193d68d917f4db797a
heber <heber@ziptransfers.com>
2025-09-02 12:13:19 +0200

    init commit for test project feat: add banner with custom text

---
+++ teamchecklist/src/main/java/com/aggiovato/teamchecklist/TeamchecklistApplication.java
@@ -0,0 +1,13 @@
+package com.aggiovato.teamchecklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamchecklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamchecklistApplication.class, args);
+	}
+
+}
---
+++ teamchecklist/src/test/java/com/aggiovato/teamchecklist/TeamchecklistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aggiovato.teamchecklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamchecklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
