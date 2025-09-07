23b3ad51650f398cac5d00e29fa403c4864589bd
Aggiovato <aggiovato@gmail.com>
2025-09-07 21:36:02 +0200

    feat: some fix to taskfile and compose dockerfiles, and some minor changes to style in front, pending to be validated

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,20 +1,25 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { httpErrorsInterceptor } from './core/http.interceptor';
 import { provideRouter } from '@angular/router';
 
 import { MessageService } from 'primeng/api';
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
     MessageService,
+    providePrimeNG({ ripple: true }),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([httpErrorsInterceptor])),
-  ]
+  ],
 };
-
